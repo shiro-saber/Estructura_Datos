466bf4e5a420084ef7ec6fd454adafc29d714789
shiro-saber <shiro@gmail.com>
2015-01-19 14:07:16 -0600

    Lista la matriz

--- Suma_Matriz/main.cpp
+++ Suma_Matriz/main.cpp
@@ -35,16 +35,16 @@ int main()
 
 	for(i = 0; i < 4; i++)
 		for(j = 0; j < 4; j++)
-			a[i][j] = rand()%2;
+			a[i][j] = rand()%564;
 			laFuncion();
 			cout << "el resultado es: " << prueba;
-			cout << endl << "LA MATRIZ 2" << endl;
+			/*cout << endl << "LA MATRIZ 2" << endl;
 
 	for(i = 0; i < 4; i++){
 		for(j = 0; j < 4; j++){
 			cout << a[i][j]<<"|";
 		}
 		cout <<endl;
-	}
+	}*/
 	return 0;
 }
