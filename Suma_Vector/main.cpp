431f8433145d9d409dcd61152bd88865be1d5574
shiro-saber <shiro@gmail.com>
2015-01-19 13:56:40 -0600

    Actualizando

--- Suma_Vector/main.cpp
+++ Suma_Vector/main.cpp
@@ -3,9 +3,16 @@
 
 using namespace std;
 
-int suma_vector()
+int suma_vector(int j[], int i, int n)
 {
-
+    if(i == n)
+    {
+        return 0;
+    }
+    else
+    {
+        return j[i] + suma(j, i + 1, n);
+    }
 }
 
 int main()
