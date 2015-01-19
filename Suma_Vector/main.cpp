2f7109f510bcff545a681bbfb01f2237a482ab11
shiro-saber <shiro@gmail.com>
2015-01-19 14:20:00 -0600

    Vector Terminado

--- Suma_Vector/main.cpp
+++ Suma_Vector/main.cpp
@@ -5,18 +5,31 @@ using namespace std;
 
 int suma_vector(int j[], int i, int n)
 {
-    if(i == n)
+    if ( i == n)
     {
         return 0;
     }
     else
     {
-        return j[i] + suma(j, i + 1, n);
+        return numeros[i] + suma(numeros, i+1, n);
     }
 }
 
 int main()
 {
+const int N = 10000;
+
+    int numeros[N];
+
+    srand((int) time(NULL));
+
+    for (int i = 0; i < N; ++i)
+    {
+        numeros[i] = rand() % 5;
+    }
+
+    cout << "Suma = " << suma(numeros, 0, N) << endl;
 
     return 0;
+
 }
