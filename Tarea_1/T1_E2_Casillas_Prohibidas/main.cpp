dd1bc8906b40f0ed4f9a0580965c01c0b60d7282
shiro-saber <shiro@gmail.com>
2015-01-29 10:57:43 -0600

    Poniendolo bonito

--- Tarea_1/T1_E2_Casillas_Prohibidas/main.cpp
+++ Tarea_1/T1_E2_Casillas_Prohibidas/main.cpp
@@ -18,10 +18,12 @@ lleve a una celda que no hayamos visitado ya, retrocedemos sobre nuestros pasos 
 camino alternativo.
 */
 
-const int MazeHeight = 9;
-const int MazeWidth = 9;
+//usando el metodo de backtraking
+const int columnas = 9;
+const int filas = 9;
 
-char Maze[MazeHeight][MazeWidth + 1] =
+//Este es el laberinto predefinido
+char laberinto[columnas][filas + 1] =
 {
 	"# #######",
 	"#   #   #",
@@ -34,85 +36,86 @@ char Maze[MazeHeight][MazeWidth + 1] =
 	"####### #",
 };
 
-const char Wall = '#';
-const char Free = ' ';
-const char SomeDude = '*';
+const char pared = '#';
+const char reiniciala = ' ';
+const char pasamos = '*';
 
-class COORD
+//Para guardar las coordenadas, actualizarlas y saber donde estoy
+class coordenadas
 {
 public:
 	int X;
 	int Y;
-	COORD(int x = 0, int y = 0) { X = x, Y = y; }
-	COORD(const COORD &coord) { X = coord.X; Y = coord.Y; }
+	coordenadas(int x = 0, int y = 0) //establecer las coordenadas donde se pasara, inicia en 0
+	{
+        X = x,
+        Y = y;
+    }
+
+	coordenadas(const coordenadas &coord) //Acutalizamos las coordenadas por las actuales
+	{
+        X = coord.X;
+        Y = coord.Y;
+	}
 };
 
-COORD StartingPoint(1, 0);
-COORD EndingPoint(7, 8);
+coordenadas inicio(1, 0); //establecemos el inicio, a lado de la primer pared
+coordenadas salida(7, 8); //establecemos donde va a acabar el laberinto
 
-void PrintDaMaze()
+void ve_laberinto()
 {
-	for (int Y = 0; Y < MazeHeight; Y++)
+	for (int Y = 0; Y < columnas; Y++)
 	{
-		printf("%s\n", Maze[Y]);
+		printf("%s\n", laberinto[Y]); //imprimimos el laberinto por partes
 	}
 	printf("\n");
 }
 
-bool Solve(int X, int Y)
+bool encuentra(int X, int Y)
 {
-	// Make the move (if it's wrong, we will backtrack later.
-	Maze[Y][X] = SomeDude;
+	// Movemos, si no se puede regresamos a una casilla anterior
+	laberinto[Y][X] = pasamos;
 
-	// If you want progressive update, uncomment these lines...
-	//PrintDaMaze();
-	//Sleep(50);
-
-	// Check if we have reached our goal.
-	if (X == EndingPoint.X && Y == EndingPoint.Y)
+	// veamos si ya llegamos al final.
+	if (X == salida.X && Y == salida.Y)
 	{
 		return true;
 	}
 
-	// Recursively search for our goal.
-	if (X > 0 && Maze[Y][X - 1] == Free && Solve(X - 1, Y))
+	//Aqui esta la recursividad, ya que estamos buscando donde esta la meta
+	if (X > 0 && laberinto[Y][X - 1] == reiniciala && encuentra(X - 1, Y))
 	{
 		return true;
 	}
-	if (X < MazeWidth && Maze[Y][X + 1] == Free && Solve(X + 1, Y))
+	if (X < filas && laberinto[Y][X + 1] == reiniciala && encuentra(X + 1, Y))
 	{
 		return true;
 	}
-	if (Y > 0 && Maze[Y - 1][X] == Free && Solve(X, Y - 1))
+	if (Y > 0 && laberinto[Y - 1][X] == reiniciala && encuentra(X, Y - 1))
 	{
 		return true;
 	}
-	if (Y < MazeHeight && Maze[Y + 1][X] == Free && Solve(X, Y + 1))
+	if (Y < columnas && laberinto[Y + 1][X] == reiniciala && encuentra(X, Y + 1))
 	{
 		return true;
 	}
 
-	// Otherwise we need to backtrack and find another solution.
-	Maze[Y][X] = Free;
+	// Si no hemos llegado, o tomamos otro camino que no era, tenemos que regresar la casilla y hacerla pasable de nuevo
+	laberinto[Y][X] = reiniciala;
 
-	// If you want progressive update, uncomment these lines...
-	//PrintDaMaze();
-	//Sleep(50);
 	return false;
 }
 
 int main()
 {
-	if (Solve(StartingPoint.X, StartingPoint.Y))
+	if (encuentra(inicio.X, inicio.Y)) //si lo resolvimos imprimimos el resultado final
 	{
-		PrintDaMaze();
+		ve_laberinto();
 	}
-	else
+	else //si no el oops es que no lo encontramos o solo no hay solucion
 	{
-		printf("Damn\n");
+		printf("Ooops!\n");
 	}
-
-	//system("PAUSE");
 
 	return 0;
 }
