31a4357085c5cc51e16db80a7e9a661f738ad680
shiro-saber <shiro.archerl@gmail.com>
2015-02-07 13:12:29 -0600

    Subiendo segundo ejercicio tarea 2

---
+++ Tarea_2/T2_E2_Arrays/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/*
+Realice un programa que genere aleatoriamente 1000 cadenas de caracteres e imprima el
+arreglo resultante de ordenarlas ascendente y descendentemente, utilizando el método de la
+clase Ordenamiento seleccionado por el usuario
+*/
+
+int main()
+{
+}
