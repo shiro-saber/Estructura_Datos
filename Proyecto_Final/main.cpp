866e4e830e9a70523c2cb1369cf74abea93bc301
shiro <shiro@debian.shiro>
2015-02-23 19:44:41 -0600

    Probando netbeans en deabia

---
+++ Proyecto_Final/main.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+using size_type = long unsigned int;
+
+int main(int argc,char* argv[])
+{
+        cout << sizeof(size_type)<<std::endl;
+        return 0;
+}
+
