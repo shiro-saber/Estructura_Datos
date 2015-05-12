e14bb8141e385706258dc11844b9fa2b9d091032
shiro-saber <shiro.archerl@gmail.com>
2015-05-12 14:21:24 -0500

    no supe como acomodarlo :(

--- Examen_Final/E2_Fractal/main.cpp
+++ Examen_Final/E2_Fractal/main.cpp
@@ -20,21 +20,25 @@ void imprimeMatriz(char m[N][N])
 
 int fractal(char a[N][N], int n)
 {
+    char temp [N][N];
     if(n != 0)
     {
         for (int i = 0; i < 9; ++i)
         {
-            if(i == 4)
-                imprimeMatriz(a);
+            if(i == 4) //cada cuadro se imprime el original que debe quedar en medio
+                imprimeMatriz(temp);
             char b[N][N];
             for (int k = 0; k < N; k++)
             {
                 for (int j = 0; j < N; j++)
                 {
-                    b[k][j] = c[n];
+                    b[k][j] = c[n]; //se crea la nueva matriz representando los cuadros que se dividen
                 }
             }
             imprimeMatriz(b);
+            for (int l = 0; l < N; l++)
+                for (int m = 0; m < N; m++)
+                    temp[l][m] = b[l][m];
         }
         fractal(a, n-1);
     }
@@ -48,7 +52,7 @@ int main(int argc, char** argv)
     {
 	for (int j = 0; j < N; j++)
 	{
-            a[i][j] = c[0];
+            a[i][j] = c[0]; //llenamos el cuadro mas grande
 	}
     }
     
